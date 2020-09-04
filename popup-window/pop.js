bccd6ec300f0e561cdebe5166226e483bcfdfa72
shlomi <shlomi@magic-flex.com>
2020-09-05 01:32:48 +0300

    add&style  modal popup window

---
+++ popup-window/pop.js
@@ -0,0 +1,26 @@
+var parent = document.querySelector(".modal-parent"),
+    btn = document.querySelector("button"),
+    X = document.querySelector(".X"),
+    section = document.querySelector("section");
+
+btn.addEventListener("click", appear);
+
+function appear() {
+    parent.style.display = "block";
+    section.style.filter = "blur(10px)";
+}
+
+X.addEventListener("click", disappearX);
+
+function disappearX() {
+    parent.style.display = "none";
+    section.style.filter = "blur(0)";
+} 
+
+parent.addEventListener("click", disappearParent);
+
+function disappearParent(e) {
+    if ( e.target.className === "modal-parent") {
+        disappearX();
+    }
+}
