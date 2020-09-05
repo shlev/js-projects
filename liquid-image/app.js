2b7bfcf0b951e383512649bcffa1bfca9108aa40
shlomi <shl.lev.ks@gmail.com>
2020-09-05 17:47:20 +0300

    finish liquid image

---
+++ liquid-image/app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './image1.jpg',
+    image2: './image2.jpg',
+    speedIn: 2,
+    speedOut: 5,
+    displacementImage: './heightMap.png'
+})
