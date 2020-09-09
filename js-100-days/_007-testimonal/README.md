4e252593723cd5c6d522f19e4acbafeb356614bc
shlomi <shl.lev.ks@gmail.com>
2020-09-09 13:51:06 +0300

    007 testimonial source

---
+++ js-100-days/_007-testimonal/README.md
@@ -0,0 +1,6 @@
+* CSS
+
+mask over background
+    background:linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)) ,url('../img/questionBcg.jpeg')center/cover no-repeat fixed;
+
+transparent
--- js-100-days/_007-testimonal/js/app.js
+++ js-100-days/_007-testimonal/js/app.js
@@ -1,77 +1,59 @@
-(function () {
+(function(){
+    const customerImage = document.querySelector('#customer-img')
+    const customerName = document.querySelector('#customer-name')
+    const customerText = document.querySelector('#customer-text')
+    const buttons = document.querySelectorAll('.btn')
+    let index = 0
+    const customers = []
 
-    class Customer {
-        constructor(imagePostfix, name, rate, quote) {
-            this.image = "./img/customer-" + imagePostfix + ".jpg";
-            this.name = name;
-            this.rate = rate;
-            this.quote = quote;
-        }
-    }
-
-    let testimoialIndex = 0;
-
-    const image = document.querySelector(".image");
-    const name = document.querySelector(".name");
-    const rate = document.querySelector(".rate");
-    const quote = document.querySelector(".quote");
-
-    const buttons = document.querySelectorAll(".btn");
-   
-    const customer0 = new Customer(0, "name1", 4.5, "Quote1")
-    const customer1 = new Customer(1, "name2", 3.5, "Quote2");
-    const customer2 = new Customer(2, "name3", 2.5, "Quote3");
-    const customer3 = new Customer(3, "name4", 1.5, "Quote4");
-    const customer4 = new Customer(4, "name5", 4, "Quote5");
+    //Create a new customer using a customer constructor
     
-    const customers = [ customer0, customer1, customer2, customer3, customer4];
-   
-
-    function updateView(customerIndex) {
-        const customer = customers[customerIndex];
-        image.style.backgroundImage = "url('" + customer.image + "')";
-        name.textContent = customer.name;
-        updateRate(customer.rate);
-        quote.textContent = customer.quote;
+    //Customer Constructor
+    function Customer(img, name, text) {
+        this.img = img
+        this.name = name
+        this.text = text
     }
 
-    updateView(testimoialIndex);
+    //Create new customer using the constructor function
 
-    buttons.forEach( btn => {
-        btn.addEventListener('click', rotateTestimonial)
+    function createCustomer(img, name, text) {
+
+        let fullImg = `./img/customer-${img}.jpg`
+        let customer = new Customer(fullImg, name, text)
+
+        customers.push(customer)
+    }
+
+    
+    createCustomer(0, 'John', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?')
+    createCustomer(1, 'Sandy', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock')
+    createCustomer(2, 'Amy', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.')
+    createCustomer(3, 'Tyrell', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
+    createCustomer(4, 'Wanda', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
+    
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            if (e.target.parentElement.classList.contains('prevBtn')){
+               if(index === 0){
+                    index = customers.length
+               }
+               index--
+               customerImage.src = customers[index].img
+               customerName.textContent = customers[index].name
+               customerText.textContent = customers[index].text
+            }
+            if (e.target.parentElement.classList.contains('nextBtn')){
+                index++
+                if(index === customers.length){
+                     index = 0
+                }
+                customerImage.src = customers[index].img
+                customerName.textContent = customers[index].name
+                customerText.textContent = customers[index].text
+             }
+        })
     })
-
-    function updateRate( rateValue) {
-        rate.innerHTML = "";
-        let starFA = getFAspan("fa-star");
-        const starCount = parseInt(rateValue/1);
-        for ( let i=0; i< starCount; i++) {
-            let starFA = getFAspan("fa-star");
-            rate.appendChild(starFA);
-        }
-
-        if ( rateValue%1 != 0) {
-            const halfStarFa = getFAspan("fa-star-half");
-            rate.appendChild(halfStarFa);
-        }
-    }
-
-    function getFAspan(faClass) {
-        const spanFA = document.createElement('span');
-        let faCode = "<i class='fas #'></i>".replace("#", faClass);
-        spanFA.innerHTML = faCode;
-        return spanFA;
-    }
-
-    function rotateTestimonial(event) {
-        if ( event.target.classList.contains('left')) testimoialIndex--;
-        if ( event.target.classList.contains('right')) testimoialIndex++;
-
-        if ( testimoialIndex > 4) testimoialIndex=0;
-        if ( testimoialIndex < 0 ) testimoialIndex=4;
-
-        updateView(testimoialIndex);
-
-    }
-
-})();
+    
+})()
