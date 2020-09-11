90902081cd247a1bfbd924152c608174603309bb
shlomi <shl.lev.ks@gmail.com>
2020-09-11 10:30:11 +0300

    done self

--- js-100-days/_008-filter-project/README.md
+++ js-100-days/_008-filter-project/README.md
@@ -5,3 +5,11 @@
 * increase size
 
 * attach icon to text input
+
+##JS
+
+get actual style
+var style = window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle;
+
+// reading properties
+console.log(style.backgroundColor);
--- js-100-days/_008-filter-project/app.js
+++ js-100-days/_008-filter-project/app.js
@@ -1,16 +1,46 @@
 ( function () {
 
     const buttons = document.querySelectorAll(".btn");
-    const images = document.querySelectorAll(".products.image");
+    const images = document.querySelectorAll(".products .image");
+    const textBox = document.querySelector(".textual input")
+    
     buttons.forEach( button => {
-        const btnText = button.textContent;
+        
         button.addEventListener('click', (e) => {
-            filterImages (btntext);
+            const btnText = e.target.textContent;
+            filterImages (btnText);
         });
     });
 
-    function filterImages(selector) {
-        images
+    textBox.addEventListener('keyup', filterByText);
+
+    function filterByText( e) {
+        const textBox = e.target;
+        alert(textBox.value);
     }
+
+    function filterImages(selector) {
+        if ( selector === 'all') {
+            images.forEach( image => showImage(image, true));
+        } else {
+            selector = selector.substring(0, 3);
+            
+            images.forEach( image => {
+                const imageBackground = window.getComputedStyle(image).background;
+                if ( imageBackground.indexOf(selector) > -1 ) {
+                    showImage(image, true);
+                } else {
+                    showImage(image, false);
+                }
+            });
+        } 
+    }
+
+    function showImage( image, show ) {
+        const parent = image.parentElement;
+        parent.style.display = show ? "block" : "none";
+    }
+
+    
 
 })()
